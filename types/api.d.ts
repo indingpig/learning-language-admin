112f067c0dccd9532b958f37010827e4df08e009
indingpig <indoing@gmail.com>
2024-12-26 16:10:24 +0800

    添加type

--- types/api.d.ts
+++ types/api.d.ts
@@ -1,6 +1,12 @@
 /** 所有 api 接口的响应数据都应该准守该格式 */
 interface ApiResponseData<T> {
   code: number
-  data: T
-  message: string
+  data: T,
+  img: string,
+  uuid: string,
+  token: string,
+  message: string,
+  username: string,
+  roles: string[],
+  user: any
 }
