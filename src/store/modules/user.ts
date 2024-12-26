b43b3f751244cb8cfe3ff2e18aa08aa9e7309c25
indingpig <indoing@gmail.com>
2024-12-26 16:09:21 +0800

    修复登陆问题

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -18,15 +18,15 @@ export const useUserStore = defineStore("user", () => {
   const settingsStore = useSettingsStore()
 
   /** 登录 */
-  const login = async ({ username, password, code }: LoginRequestData) => {
-    const { data } = await loginApi({ username, password, code })
+  const login = async ({ username, password, code, uuid }: LoginRequestData) => {
+    const data = await loginApi({ username, password, code, uuid })
     setToken(data.token)
     token.value = data.token
   }
   /** 获取用户详情 */
   const getInfo = async () => {
-    const { data } = await getUserInfoApi()
-    username.value = data.username
+    const data = await getUserInfoApi()
+    username.value = data.user.nickName
     // 验证返回的 roles 是否为一个非空数组，否则塞入一个没有任何作用的默认角色，防止路由守卫逻辑进入无限循环
     roles.value = data.roles?.length > 0 ? data.roles : routeSettings.defaultRoles
   }
