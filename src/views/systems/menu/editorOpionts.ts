ef53cf3fbe1dabcee2c00873da7c6fd1d998a4a4
indingpig <indoing@gmail.com>
2025-01-03 16:11:27 +0800

    提取代码

---
+++ src/views/systems/menu/editorOpionts.ts
@@ -0,0 +1,19 @@
+const editorOptions = {
+  placeholder: '请输入内容',
+  modules: {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block'],
+      [{ 'header': 1 }, { 'header': 2 }],
+      [{ 'script': 'sub' }, { 'script': 'super' }],
+      [{ 'indent': '-1' }, { 'indent': '+1' }],
+      [{ 'direction': 'rtl' }],
+      [{ 'size': ['small', false, 'large', 'huge'] }],
+      [{ 'color': [] }, { 'background': [] }],
+      [{ 'font': [] }],
+      [{ 'align': [] }]
+    ]
+  }
+}
+
+export default editorOptions
