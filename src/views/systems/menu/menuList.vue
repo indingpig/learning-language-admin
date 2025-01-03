2e62bcbc7aa4446873b890610bb38403e8db8b8b
indingpig <indoing@gmail.com>
2025-01-03 16:11:53 +0800

    测试代码

--- src/views/systems/menu/menuList.vue
+++ src/views/systems/menu/menuList.vue
@@ -6,18 +6,19 @@
       </el-col>
     </el-row>
     <el-table :data="tableData" stripe border>
-      <el-table-column prop="catalogName" label="主题名称"></el-table-column>
-      <el-table-column prop="catalogDesc" label="描述"></el-table-column>
+      <el-table-column prop="subjectName" label="主题名称"></el-table-column>
+      <el-table-column prop="subjectDesc" label="描述"></el-table-column>
       <el-table-column prop="isExpired" label="是否过期">
         <template #default="{ row }">
           <el-tag v-if="row.isExpired === 'Y'" type="danger">是</el-tag>
           <el-tag v-if="row.isExpired === 'N'" type="success">否</el-tag>
+          <el-tag v-if="row.isExpired === null" type="primary">---</el-tag>
         </template>
       </el-table-column>
       <el-table-column label="操作">
         <template #default="{ row }">
           <el-button link type="primary" size="small" @click="openEdit(row)">编辑</el-button>
-          <el-button link type="primary" size="small">删除</el-button>
+          <el-button link type="danger" size="small">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -45,28 +46,15 @@ const addNewTheme = () => {
 
 const handleAddSuccess = () => {
   console.log('新增主题成功');
+  getMenu();
 };
 
 const openEdit = (row: any) => {
-  router.push({ name: 'menuDetail', params: { catalogId: row.catalogId } });
+  router.push({ name: 'menuDetail', params: { subjectId: row.subjectId, subjectDesc: row.subjectDesc, subjectName: row.subjectName } });
 };
 
 const getMenu = () => {
   getUserMenuApi().then(data => {
-    data.rows = [
-      {
-        catalogId: 1,
-        catalogName: '主题1',
-        catalogDesc: '主题1描述',
-        isExpired: 'Y'
-      },
-      {
-        catalogId: 2,
-        catalogName: '主题2',
-        catalogDesc: '主题2描述',
-        isExpired: 'N'
-      }
-    ]
     tableData.value = data.rows;
   });
 };
