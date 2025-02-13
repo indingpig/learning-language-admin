e4f436d3d70c87a73bed13fd576ad7a6687bc36c
indingpig <indoing@gmail.com>
2025-02-13 15:03:10 +0800

    去掉警告

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,11 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
         "@": resolve(__dirname, "./src")
       }
     },
+    css: {
+      preprocessorOptions: {
+        scss: { api: 'modern-compiler' },
+      }
+    },
     server: {
       /** 设置 host: true 才可以使用 Network 的形式，以 IP 访问项目 */
       host: true, // host: "0.0.0.0"
